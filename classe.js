b9b5a605422144163b15f55220a30c2688209068
kajal verma <kajalverma7355@gmail.com>
2025-02-18 12:35:16 +0530

    first commit

--- classe.js
+++ classe.js
@@ -21,48 +21,63 @@
 // obj.running() 
 
  ///  constructor
-class person{
-    monkey;
-    name;
-    surname
-    constructor(friend, firstName, surName ){
-        this.monkey = friend,
-        this.name = firstName,
-        this.surname = surName
+// class person{
+//     monkey;
+//     name;
+//     surname
+//     constructor(friend, firstName, surName ){
+//         this.monkey = friend,
+//         this.name = firstName,
+//         this.surname = surName
+//     }
+
+//     // getMyName(){
+//     //     console.log("Heloo kajal")
+//     // }
+// }
+// let obj = new person("anajli","kajal","verma")
+// console.log(obj.monkey)
+// console.log(obj.surname)
+// console.log(obj.name)
+// // obj.getMyName()
+
+
+// /// default values in function
+
+// function sayName(name = "kajal"){
+//     console.log("my first name :",name )
+// }
+
+// sayName() // kyoki mene yaha koi value pass nhi ki hun agr karti to sayname ka chalta fucntion 
+
+
+// function sayVerma(first = "verma", lastName = first.toUpperCase()){
+//     console.log("------------",first," ", lastName)
+// }
+// sayVerma()
+
+
+// function kajal(value = { age: 12, wt: 12 }) {
+//     console.log(".................", value);
+// }
+// kajal();
+
+// function ram(value = ["kajal","anjali"]){
+//     console.log("------------------",value)
+// }
+// ram()
+
+class getMyProfile{
+    name="kajal";
+    age=13
+
+    wellDone(){
+        var name = "ashish"
+        console.log("my name is!",name)
     }
-
-    // getMyName(){
-    //     console.log("Heloo kajal")
-    // }
-}
-let obj = new person("anajli","kajal","verma")
-console.log(obj.monkey)
-console.log(obj.surname)
-console.log(obj.name)
-// obj.getMyName()
-
-
-/// default values in function
-
-function sayName(name = "kajal"){
-    console.log("my first name :",name )
 }
 
-sayName() // kyoki mene yaha koi value pass nhi ki hun agr karti to sayname ka chalta fucntion 
-
-
-function sayVerma(first = "verma", lastName = first.toUpperCase()){
-    console.log("------------",first," ", lastName)
-}
-sayVerma()
-
-
-function kajal(value = { age: 12, wt: 12 }) {
-    console.log(".................", value);
-}
-kajal();
-
-function ram(value = ["kajal","anjali"]){
-    console.log("------------------",value)
-}
-ram()
+let profile = new getMyProfile()
+console.log(profile.age)
+console.log(profile.name)
+profile.wellDone()
