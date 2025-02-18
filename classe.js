3c82ff14fbe8bceeec19065b2f3c48d2652738e1
kajal verma <kajalverma7355@gmail.com>
2025-02-18 12:37:36 +0530

    first commit

--- classe.js
+++ classe.js
@@ -1,71 +1,71 @@
-// class human {
-//     age = 12;  /// public
-//     #wt = 13; /// private 
-//     ht = 34;
+class human {
+    age = 12;  /// public
+    #wt = 13; /// private 
+    ht = 34;
 
-//     walking(){
-//         var age = 18
-//         console.log("i am walking !",this.#wt)
-//         console.log("my age is !", age )
-//     }
+    walking(){
+        var age = 18
+        console.log("i am walking !",this.#wt)
+        console.log("my age is !", age )
+    }
 
-//     running(){
-//         const myName = "kajal"
-//         console.log("my name is !",myName)
-//     }
-// }
-// let obj = new human()  /// create a new object 
-// console.log(obj.age)
+    running(){
+        const myName = "kajal"
+        console.log("my name is !",myName)
+    }
+}
+let obj1 = new human()  /// create a new object 
+console.log(obj1.age)
 
-// obj.walking();
-// obj.running() 
+obj1.walking();
+obj1.running() 
 
- ///  constructor
-// class person{
-//     monkey;
-//     name;
-//     surname
-//     constructor(friend, firstName, surName ){
-//         this.monkey = friend,
-//         this.name = firstName,
-//         this.surname = surName
-//     }
+ /  constructor
+class person{
+    monkey;
+    name;
+    surname
+    constructor(friend, firstName, surName ){
+        this.monkey = friend,
+        this.name = firstName,
+        this.surname = surName
+    }
 
-//     // getMyName(){
-//     //     console.log("Heloo kajal")
-//     // }
-// }
-// let obj = new person("anajli","kajal","verma")
-// console.log(obj.monkey)
-// console.log(obj.surname)
-// console.log(obj.name)
-// // obj.getMyName()
-
-
-// /// default values in function
-
-// function sayName(name = "kajal"){
-//     console.log("my first name :",name )
-// }
-
-// sayName() // kyoki mene yaha koi value pass nhi ki hun agr karti to sayname ka chalta fucntion 
+    // getMyName(){
+    //     console.log("Heloo kajal")
+    // }
+}
+let obj = new person("anajli","kajal","verma")
+console.log(obj.monkey)
+console.log(obj.surname)
+console.log(obj.name)
+// obj.getMyName()
 
 
-// function sayVerma(first = "verma", lastName = first.toUpperCase()){
-//     console.log("------------",first," ", lastName)
-// }
-// sayVerma()
+/// default values in function
+
+function sayName(name = "kajal"){
+    console.log("my first name :",name )
+}
+
+sayName() // kyoki mene yaha koi value pass nhi ki hun agr karti to sayname ka chalta fucntion 
 
 
-// function kajal(value = { age: 12, wt: 12 }) {
-//     console.log(".................", value);
-// }
-// kajal();
+function sayVerma(first = "verma", lastName = first.toUpperCase()){
+    console.log("------------",first," ", lastName)
+}
+sayVerma()
 
-// function ram(value = ["kajal","anjali"]){
-//     console.log("------------------",value)
-// }
-// ram()
+
+function kajal(value = { age: 12, wt: 12 }) {
+    console.log(".................", value);
+}
+kajal();
+
+function ram(value = ["kajal","anjali"]){
+    console.log("------------------",value)
+}
+ram()
 
 class getMyProfile{
     name="kajal";
